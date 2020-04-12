9ee748ba0846519b565142b233dcd91f940a9cf5
pmattd <petermatthewduncan@gmail.com>
2020-04-12 22:17:07 +0200

    first tests

---
+++ tests/DataFileTest.py
@@ -0,0 +1,16 @@
+import unittest
+
+from data_generator.DataFile import EnumeratedFieldGenerator, DataLineGenerator
+
+
+class DataFileTest(unittest.TestCase):
+
+    def testEnumeratedFieldGenerator(self):
+        generator = EnumeratedFieldGenerator(["hello"])
+        self.assertEqual(generator.getVal(), "hello")
+
+    def testGenerate(self):
+        generator1 = EnumeratedFieldGenerator(["hello"])
+        generator2 = EnumeratedFieldGenerator(["goodbye"])
+        line_generator = DataLineGenerator([generator1, generator2], ",")
+        self.assertEqual(line_generator.generate(), "hello,goodbye")
